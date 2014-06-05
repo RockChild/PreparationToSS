ce0839bb4487ad899f67439fd90faba59514317f
Pasha <pavel.shtefanesku@gmail.com>
2014-06-05 18:27:39 +0300

    Update Factorial.java
    Cleaning code.

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,22 @@
 package vik1;
 public class Factorial {
 	
-	static int fact(int n){      //recursia
-	if (n==1) return 1;			 //method
-		int sum=fact(n-1)*n;	 //to find
-		return sum;				 //factorial
+	static int fact(int n){ 				 //recursia
+	if (n==1) return 1;					 //method
+		int sum=fact(n-1)*n;				 //to find
+		return sum;					 //factorial
+	}
+	int fact_recurent(int n){
+		int sum=1;					//recurent
+		for(int i=1;i<=n;i++)				//algorithm
+			sum=i*sum;				//to find
+		System.out.println(n+"! = "+ sum); 		//factorial
+		return sum;
 	}
 	
 	public static void main(String[]args){
 		int n=5;
-		System.out.println(n+"! = "+ fact(n));//calling 1-th method
-		
-		int sum=1;							//recurent
-		for(int i=1;i<=n;i++)				//algorithm
-			sum=i*sum;						//to find
-		System.out.println(n+"! = "+ sum);	//factorial
-		
-
+		System.out.println(n+"! = "+ fact(n));		//calling 1-th method
+		System.out.println(n+"! = "+fact_recurent(n));	//calling 2-nd method
 	}
 }
