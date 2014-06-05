2db27805cfd1b8a92b26ad7d3322cf61f5e0dad8
Pasha <pavel.shtefanesku@gmail.com>
2014-06-05 19:03:19 +0300

    Cleaning

--- Factorial.java
+++ Factorial.java
@@ -1,22 +1,20 @@
-package vik1;
 public class Factorial {
 	
-	static int fact(int n){ 				 //recursia
+	public static int fact(int n){ 				 //recursia
 	if (n==1) return 1;					 //method
 		int sum=fact(n-1)*n;				 //to find
 		return sum;					 //factorial
 	}
-	int fact_recurent(int n){
+	public static int fact_recurent(int n){
 		int sum=1;					//recurent
 		for(int i=1;i<=n;i++)				//algorithm
-			sum=i*sum;				//to find
-		System.out.println(n+"! = "+ sum); 		//factorial
+			sum=i*sum;				//to find fact
 		return sum;
 	}
 	
 	public static void main(String[]args){
 		int n=5;
-		System.out.println(n+"! = "+ fact(n));		//calling 1-th method
+		System.out.println(n+"! = "+fact(n));		//calling 1-th method
 		System.out.println(n+"! = "+fact_recurent(n));	//calling 2-nd method
 	}
 }
